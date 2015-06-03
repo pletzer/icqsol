78339f0c33e9d8315afbab147e4f993434ead3f5
ALEXANDER PLETZER <axp5530@psu.edu>
2015-06-03 15:33:00 -0400

    + tests

---
+++ tests/tools/geometry/testPipe.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""
+Test simple pipe object
+@author pletzer@psu.edu
+"""
+
+from icqsol.tools.geometry.sqGeometry import Geometry
+from icqsol.tools.geometry.sqCylinder import Cylinder
+
+bxLo = (0., 0., 0.)
+bxHi = (1., 1., 1.)
+geom = Geometry(bxLo=bxLo, bxHi=bxHi, full=False)
+geom += Cylinder(radius=0.4, origin=(0.5, 0.5, 0.5))
+geom -= Cylinder(radius=0.2, origin=(0.5, 0.5, 0.5))
+geom.show()
