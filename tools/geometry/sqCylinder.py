bbe216f8dca00b8d34de431a938c73b0690ce163
ALEXANDER PLETZER <axp5530@psu.edu>
2015-06-04 09:56:19 -0400

    now able to create a ring

--- tools/geometry/sqCylinder.py
+++ tools/geometry/sqCylinder.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 
 import vtk
+import numpy
 
-class Cylinder(vtk.vtkCylinder):
-
-  EPS = 1.2345678e-10
+class Cylinder(vtk.vtkImplicitBoolean):
 
   def __init__(self, radius, origin, length):
     """
@@ -13,12 +12,26 @@ class Cylinder(vtk.vtkCylinder):
     @param origin center of the cylinder
     @param length length of the cylinder
     """
+    self.cyl = vtk.vtkCylinder()
+    self.cyl.SetRadius(radius)
+    self.cyl.SetCenter(origin)
 
-    self.SetRadius(radius)
-    self.SetCenter(origin)
+    self.cylTransform = vtk.vtkGeneralTransform()
+    self.cylTransform.RotateX(90.0)
+    self.cyl.SetTransform(self.cylTransform)
 
+    # cut the cylinder
+    self.planeLo = vtk.vtkPlane()
+    self.planeLo.SetOrigin(origin[0], origin[1], origin[2] - 0.5*length)
+    self.planeLo.SetNormal(0., 0., -1.)
 
+    self.planeHi = vtk.vtkPlane()
+    self.planeHi.SetOrigin(origin[0], origin[1], origin[2] + 0.5*length)
+    self.planeHi.SetNormal(0., 0., 1.)
 
-
-
+    # combine
+    self.SetOperationTypeToIntersection()
+    self.AddFunction(self.cyl)
+    self.AddFunction(self.planeLo)
+    self.AddFunction(self.planeHi)
 
