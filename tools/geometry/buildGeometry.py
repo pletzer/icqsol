9be013f4742dc96b4af2ba85678ced93afa292b0
ALEXANDER PLETZER <axp5530@psu.edu>
2015-06-07 20:58:54 -0400

    started work on buildGeometry

--- tools/geometry/buildGeometry.py
+++ tools/geometry/buildGeometry.py
@@ -4,7 +4,6 @@ import argparse
 import time
 import re
 
-from icqsol.tools.geometry.icqGeometry import Geometry
 from icqsol.tools.geometry.icqBox import Box
 from icqsol.tools.geometry.icqSphere import Sphere
 from icqsol.tools.geometry.icqCylinder import Cylinder
@@ -15,10 +14,10 @@ tid = re.sub(r'\.', '', time.time())
 parser = argparse.ArgumentParser(description='Build geometry.')
 
 parser.add_argument('--create', dest='createExprs', nargs='+', 
-	help='Create shape, e.g. "s = Cylinder(radius=0.1, origin=[0., 0., 0.])"')
+	help='Create shape, e.g. "c = Cylinder(radius=0.1, origin=[0., 0., 0.])"')
 
 parser.add_argument('--assemble', dest='assembleExpr', nargs=1, 
-	help='Assemble geometry, e.g. "(s + s2)*s3 - s4"')
+	help='Assemble geometry, e.g. "(c1 + s2)*s3 - s4"')
 
 parser.add_argument('--output', dest='output', default='buildGeometry-{0}'.format(tid), 
 	help='Output file.')
@@ -33,9 +32,8 @@ for shapeExpr in args.createExprs:
 # TO DO 
 
 # assemble the shapes
-geom = Geometry()
-geom.apply(assembleExpr)
+geom = eval(args.assembleExpr)
 
 # save
-geom
+# TO DO 
 
