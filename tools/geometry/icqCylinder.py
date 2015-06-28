c82121d2bdbd705fb5b6f218e4e1eb56ad0915cd
ALEXANDER PLETZER <axp5530@psu.edu>
2015-06-28 14:08:37 -0400

    work in progress

--- tools/geometry/icqCylinder.py
+++ tools/geometry/icqCylinder.py
@@ -24,77 +24,19 @@ class Cylinder(Shape):
     
     self.pointList = []
     self.connectivityList = []
-
-    # create the outer cylinder
-    # u ~ theta, v ~ z
-    uu, zz = uniformGrid2D([0., 0.], [2*numpy.pi, length], (n_theta, n_z))
-    xx = radius*numpy.cos(uu)
-    yy = radius*numpy.sin(uu)
-    xx += origin[0]
-    yy += origin[1]
-    zz += origin[2]
-    numPoints = len(xx.flat)
-    pointsOuter = numpy.zeros( (numPoints, 3), numpy.float32 )
-    pointsOuter[:, 0] = xx.flat
-    pointsOuter[:, 1] = yy.flat
-    pointsOuter[:, 2] = zz.flat
     
-    # cell connectivity
-    numQuadCells = n_theta * n_z
-    numCells = 2 * numQuadCells
-    iiTheta, jjZ = uniformIndexGrid2D((0, 0), (n_theta, n_z))
-    bigII = iiTheta*(n_z + 1) + jjZ
-    
-    connectivityOuter = numpy.zeros( (numCells, 4), numpy.int )
-    
-    # lower triangles
-    connectivityOuter[:numQuadCells, 1] = numpy.ravel(bigII[:-1, :-1])
-    connectivityOuter[:numQuadCells, 2] = numpy.ravel(bigII[1:, :-1])
-    connectivityOuter[:numQuadCells, 3] = numpy.ravel(bigII[1:, 1:])
-    # upper triangles
-    connectivityOuter[numQuadCells:, 1] = numpy.ravel(bigII[:-1, :-1])
-    connectivityOuter[numQuadCells:, 2] = numpy.ravel(bigII[1:, 1:])
-    connectivityOuter[numQuadCells:, 3] = numpy.ravel(bigII[:-1, 1:])
-    connectivityOuter[:, 0] = 3 # triangles
-    
-    # make sure to save the point list and connectivity list
+    # create outer, side mesh
+    pointsOuter, connectivityOuter = self._createSideMesh(origin, radius, length,
+                                                          n_theta=n_theta,
+                                                          n_z=n_z)
     self.pointList.append(pointsOuter)
     self.connectivityList.append(connectivityOuter)
     
-    # create the low end disk
-    # u ~ rho, v ~ theta
-    uu, vv = uniformGrid2D([0., 0.], [radius, 2*numpy.pi], (n_rho, n_theta))
-    xx = uu*numpy.cos(vv)
-    yy = uu*numpy.sin(vv)
-    zz = numpy.zeros( xx.shape, xx.dtype )
-    xx += origin[0]
-    yy += origin[1]
-    zz += origin[2]
-    numPoints = len(xx.flat)
-    pointsLo = numpy.zeros( (numPoints, 3), numpy.float32 )
-    pointsLo[:, 0] = xx.flat
-    pointsLo[:, 1] = yy.flat
-    pointsLo[:, 2] = zz.flat
     
-    # cell connectivity
-    numQuadCells = n_rho * n_theta
-    numCells = 2 * numQuadCells
-    iiRho, jjTheta = uniformIndexGrid2D((0, 0), (n_rho, n_theta))
-    bigII = iiRho*(n_theta + 1) + jjTheta
-    
-    connectivityLo = numpy.zeros( (numCells, 4), numpy.int )
-    
-    # lower triangles
-    connectivityLo[:numQuadCells, 1] = numpy.ravel(bigII[:-1, :-1])
-    connectivityLo[:numQuadCells, 2] = numpy.ravel(bigII[1:, :-1])
-    connectivityLo[:numQuadCells, 3] = numpy.ravel(bigII[1:, 1:])
-    # upper triangles
-    connectivityLo[numQuadCells:, 1] = numpy.ravel(bigII[:-1, :-1])
-    connectivityLo[numQuadCells:, 2] = numpy.ravel(bigII[1:, 1:])
-    connectivityLo[numQuadCells:, 3] = numpy.ravel(bigII[:-1, 1:])
-    connectivityLo[:, 0] = 3 # triangles
-    
-    # make sure to save the point list and connectivity list
+    # create low end side mesh
+    pointsLo, connectivityLo = self._createLoMesh(origin, radius,
+                                                  n_rho=n_rho,
+                                                  n_theta=n_theta)
     self.pointList.append(pointsLo)
     self.connectivityList.append(connectivityLo)
 
@@ -132,6 +74,94 @@ class Cylinder(Shape):
     self.cellInds.SetVoidArray(allConnectivity, totNumCells*4, 1)
     
     self.cells.SetCells(totNumCells, self.cellInds)
+
+  def _createSideMesh(self, origin, radius, length, n_theta, n_z):
+    """
+    Create side mesh
+    @param origin origin of the disk on the lower end
+    @param radius radius
+    @param length cylinder length
+    @param n_theta number of poloidal cells
+    @param n_z number of z cells
+    @return points, connectivity
+    """
+    # u ~ theta, v ~ z
+    uu, zz = uniformGrid2D([0., 0.], [2*numpy.pi, length], (n_theta, n_z))
+    xx = radius*numpy.cos(uu)
+    yy = radius*numpy.sin(uu)
+    xx += origin[0]
+    yy += origin[1]
+    zz += origin[2]
+    numPoints = len(xx.flat)
+    points = numpy.zeros( (numPoints, 3), numpy.float32 )
+    points[:, 0] = xx.flat
+    points[:, 1] = yy.flat
+    points[:, 2] = zz.flat
+      
+    # cell connectivity
+    numQuadCells = n_theta * n_z
+    numCells = 2 * numQuadCells
+    iiTheta, jjZ = uniformIndexGrid2D((0, 0), (n_theta, n_z))
+    bigII = iiTheta*(n_z + 1) + jjZ
+      
+    connectivity = numpy.zeros( (numCells, 4), numpy.int )
+      
+    # lower triangles
+    connectivity[:numQuadCells, 1] = numpy.ravel(bigII[:-1, :-1])
+    connectivity[:numQuadCells, 2] = numpy.ravel(bigII[1:, :-1])
+    connectivity[:numQuadCells, 3] = numpy.ravel(bigII[1:, 1:])
+    
+    # upper triangles
+    connectivity[numQuadCells:, 1] = numpy.ravel(bigII[:-1, :-1])
+    connectivity[numQuadCells:, 2] = numpy.ravel(bigII[1:, 1:])
+    connectivity[numQuadCells:, 3] = numpy.ravel(bigII[:-1, 1:])
+    connectivity[:, 0] = 3 # triangles
+
+    return points, connectivity
+
+  def _createLoMesh(self, origin, radius, n_rho, n_theta):
+    """
+    Create low end disk mesh
+    @param origin origin of the disk on the lower end
+    @param radius radius
+    @param n_rho number of radial cells
+    @param n_theta number of poloidal cells
+    @return points, connectivity
+    """
+    # u ~ rho, v ~ theta
+    uu, vv = uniformGrid2D([0., 0.], [radius, 2*numpy.pi], (n_rho, n_theta))
+    xx = uu*numpy.cos(vv)
+    yy = uu*numpy.sin(vv)
+    zz = numpy.zeros( xx.shape, xx.dtype )
+    xx += origin[0]
+    yy += origin[1]
+    zz += origin[2]
+    numPoints = len(xx.flat)
+    points = numpy.zeros( (numPoints, 3), numpy.float32 )
+    points[:, 0] = xx.flat
+    points[:, 1] = yy.flat
+    points[:, 2] = zz.flat
+        
+    # cell connectivity
+    numQuadCells = n_rho * n_theta
+    numCells = 2 * numQuadCells
+    iiRho, jjTheta = uniformIndexGrid2D((0, 0), (n_rho, n_theta))
+    bigII = iiRho*(n_theta + 1) + jjTheta
+        
+    connectivity = numpy.zeros( (numCells, 4), numpy.int )
+        
+    # lower triangles
+    connectivity[:numQuadCells, 1] = numpy.ravel(bigII[:-1, :-1])
+    connectivity[:numQuadCells, 2] = numpy.ravel(bigII[1:, :-1])
+    connectivity[:numQuadCells, 3] = numpy.ravel(bigII[1:, 1:])
+        
+    # upper triangles
+    connectivity[numQuadCells:, 1] = numpy.ravel(bigII[:-1, :-1])
+    connectivity[numQuadCells:, 2] = numpy.ravel(bigII[1:, 1:])
+    connectivity[numQuadCells:, 3] = numpy.ravel(bigII[:-1, 1:])
+    connectivity[:, 0] = 3 # triangles
+
+    return points, connectivity
 
 ################################################################################
 def test():
