74c6aa54e76c13c3a05a5ffdffa5d69ade5e51f5
ALEXANDER PLETZER <axp5530@psu.edu>
2015-06-28 14:22:25 -0400

    crashes for n_rho=3

--- tools/geometry/icqCylinder.py
+++ tools/geometry/icqCylinder.py
@@ -34,12 +34,19 @@ class Cylinder(Shape):
     
     
     # create low end side mesh
-    pointsLo, connectivityLo = self._createLoMesh(origin, radius,
+    pointsLo, connectivityLo = self._createLoMesh(origin, radius, length,
                                                   n_rho=n_rho,
                                                   n_theta=n_theta)
     self.pointList.append(pointsLo)
     self.connectivityList.append(connectivityLo)
 
+    # create high end side mesh
+    pointsHi, connectivityHi = self._createHiMesh(origin, radius, length,
+                                                  n_rho=n_rho,
+                                                  n_theta=n_theta)
+    self.pointList.append(pointsHi)
+    self.connectivityList.append(connectivityHi)
+    
     # concatenate all the meshes
     totNumPoints = 0
     totNumCells = 0
@@ -119,11 +126,12 @@ class Cylinder(Shape):
 
     return points, connectivity
 
-  def _createLoMesh(self, origin, radius, n_rho, n_theta):
+  def _createLoMesh(self, origin, radius, length, n_rho, n_theta):
     """
     Create low end disk mesh
     @param origin origin of the disk on the lower end
     @param radius radius
+    @param length cylinder length
     @param n_rho number of radial cells
     @param n_theta number of poloidal cells
     @return points, connectivity
@@ -163,11 +171,56 @@ class Cylinder(Shape):
 
     return points, connectivity
 
+  def _createHiMesh(self, origin, radius, length, n_rho, n_theta):
+    """
+    Create low end disk mesh
+    @param origin origin of the disk on the lower end
+    @param radius radius
+    @param length cylinder length
+    @param n_rho number of radial cells
+    @param n_theta number of poloidal cells
+    @return points, connectivity
+    """
+    # u ~ rho, v ~ theta
+    uu, vv = uniformGrid2D([0., 0.], [radius, 2*numpy.pi], (n_rho, n_theta))
+    xx = uu*numpy.cos(vv)
+    yy = uu*numpy.sin(vv)
+    zz = length*numpy.ones( xx.shape, xx.dtype )
+    xx += origin[0]
+    yy += origin[1]
+    zz += origin[2]
+    numPoints = len(xx.flat)
+    points = numpy.zeros( (numPoints, 3), numpy.float32 )
+    points[:, 0] = xx.flat
+    points[:, 1] = yy.flat
+    points[:, 2] = zz.flat
+    
+    # cell connectivity
+    numQuadCells = n_rho * n_theta
+    numCells = 2 * numQuadCells
+    iiRho, jjTheta = uniformIndexGrid2D((0, 0), (n_rho, n_theta))
+    bigII = iiRho*(n_theta + 1) + jjTheta
+    
+    connectivity = numpy.zeros( (numCells, 4), numpy.int )
+    
+    # lower triangles
+    connectivity[:numQuadCells, 1] = numpy.ravel(bigII[:-1, :-1])
+    connectivity[:numQuadCells, 2] = numpy.ravel(bigII[1:, 1:])
+    connectivity[:numQuadCells, 3] = numpy.ravel(bigII[1:, :-1])
+    
+    # upper triangles
+    connectivity[numQuadCells:, 1] = numpy.ravel(bigII[:-1, :-1])
+    connectivity[numQuadCells:, 2] = numpy.ravel(bigII[:-1, 1:])
+    connectivity[numQuadCells:, 3] = numpy.ravel(bigII[1:, 1:])
+    connectivity[:, 0] = 3 # triangles
+    
+    return points, connectivity
+
 ################################################################################
 def test():
 
   cyl = Cylinder(radius=1.0, origin=(0., 0., 0.), length=1.,
-                 n_rho=2, n_theta=4, n_z=1)
+                 n_rho=2, n_theta=6, n_z=2)
   cyl.save('cyl.vtk', file_format='vtk', file_type='ascii')
   #cyl.show()
 
