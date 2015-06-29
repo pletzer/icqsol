a7fdd0dff5dd09e335948a3bfe541cbcc2bce537
ALEXANDER PLETZER <axp5530@psu.edu>
2015-06-29 18:57:19 -0400

    + test subtraction of spheres

---
+++ tests/tools/geometry/testSubtraction.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+"""
+Test intersection operation
+@author pletzer@psu.edu
+"""
+
+from icqsol.tools.geometry.icqSphere import Sphere
+
+s1 = Sphere(radius=1.0, origin=(0., 0., 0.))
+s2 = Sphere(radius=1.0, origin=(1., 0., 0.))
+geom = s1 - s2
+geom.show() #filename='testSubtraction.png')
