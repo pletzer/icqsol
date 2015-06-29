b98fd736e07f67960eed9d49062b3195f430691f
ALEXANDER PLETZER <axp5530@psu.edu>
2015-06-29 19:33:30 -0400

    another fix of the normals

---
+++ tests/tools/geometry/testIntersectionCylinders.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+"""
+Test intersection operation
+@author pletzer@psu.edu
+"""
+
+from icqsol.tools.geometry.icqCylinder import Cylinder
+
+s1 = Cylinder(radius=1.0, origin=(0., 0., 0.5), length=0.5)
+s2 = Cylinder(radius=0.5, origin=(0., 0., 0.), length=1.0)
+geom = s1 * s2
+geom.show() #filename='testIntersectionCylinders.png')
