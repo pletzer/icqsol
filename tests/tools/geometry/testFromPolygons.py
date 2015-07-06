9250df308236c98af1a3aa642c339fc480eca60a
ALEXANDER PLETZER <axp5530@psu.edu>
2015-07-06 13:48:28 -0400

    + comments

--- tests/tools/geometry/testFromPolygons.py
+++ tests/tools/geometry/testFromPolygons.py
@@ -1,6 +1,10 @@
 from icqsol.tools.geometry.icqShape import Shape
-
 from csg.core import CSG
+
+"""
+Test construction of shape from a list of polygons
+@author pletzer@psu.edu
+"""
 
 cube = CSG.cube()
 shp = Shape()
--- tests/tools/geometry/testToPolygons.py
+++ tests/tools/geometry/testToPolygons.py
@@ -1,5 +1,10 @@
 from icqsol.tools.geometry.icqSphere import Sphere
 
+"""
+Test conversion from a shape to a list of polygons
+@author pletzer@psu.edu
+"""
+
 sphere = Sphere(origin = [0., 0., 0.], radius = 1.0)
 print sphere.toPolygons()
 
