598d98280629c150b41ab50dd9ec18a7ff29ad0f
ALEXANDER PLETZER <axp5530@psu.edu>
2015-07-13 09:30:45 -0400

    working on a fix for testRing

--- tests/tools/geometry/testRing.py
+++ tests/tools/geometry/testRing.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
 """
-Test creation of simple ring shape object
+Test creation of simple ring object
 @author pletzer@psu.edu
 """
 
 from icqsol.tools.geometry.icqCylinder import Cylinder
 
-c1 = Cylinder(radius=0.4, origin=(0., 0., 0.), length=0.5, n_z=12)
-c2 = Cylinder(radius=0.2, origin=(0., 0., -0.1), length=1.0, n_z=30)
-geom = c1 + c2
+c1 = Cylinder(radius=0.4, origin=(0., 0., 0.), 
+              lengths=(0.5, 0., 0.))
+c2 = Cylinder(radius=0.2, origin=(0., 0., -0.1), 
+              lengths=(1.0, 0., 0.))
+geom = c1 - c2
 geom.show() #filename='testRing.png')
