d9bacba1f0b38689630133933ecc35362e407dca
ALEXANDER PLETZER <axp5530@psu.edu>
2015-07-24 17:57:50 -0400

    flake8 compliant

--- tools/geometry/rotateShape.py
+++ tools/geometry/rotateShape.py
@@ -8,7 +8,6 @@ import argparse
 import time
 import sys
 import re
-import numpy
 
 from icqsol.tools.geometry.icqBaseShape import BaseShape
 
@@ -18,40 +17,40 @@ tid = re.sub(r'\.', '', str(time.time()))
 parser = argparse.ArgumentParser(description='Translate shape.')
 
 parser.add_argument('--input', dest='input', default='',
-  help='List of input files (PLY or VTK)')
+                    help='List of input files (PLY or VTK)')
 
 parser.add_argument('--angle', dest='angle', type=float, default=0.0,
-	help='Specify rotation angle in degrees')
+                    help='Specify rotation angle in degrees')
 
-parser.add_argument('--origin', dest='origin', default="0.,0.,0.",
-	help='Specify rotation point as three floating point numbers')
+parser.add_argument('--origin', dest='origin', default="0., 0., 0.",
+                    help='Specify rotation point (3 floating point numbers)')
 
-parser.add_argument('--axis', dest='axis', default="0.,0.,1.",
-	help='Specify rotation axis as three floating point numbers')
+parser.add_argument('--axis', dest='axis', default="0., 0., 1.",
+                    help='Specify rotation axis (3 floating point numbers)')
 
 parser.add_argument('--ascii', dest='ascii', action='store_true',
-  help='Save data in ASCII format (default is binary)')
+                    help='Save data in ASCII format (default is binary)')
 
-parser.add_argument('--output', dest='output', 
-  default='createCompositeShape-{0}.vtk'.format(tid), 
-	help='Output file.')
+parser.add_argument('--output',
+                    dest='output',
+                    default='createCompositeShape-{0}.vtk'.format(tid),
+                    help='Output file.')
 
 args = parser.parse_args()
 
 if not args.input:
-  print 'ERROR: must specify one input file with --input <file>'
-  sys.exit(3)
+    print 'ERROR: must specify one input file with --input <file>'
+    sys.exit(3)
 
 shp = BaseShape()
 origin = eval(args.origin)
 axis = eval(args.axis)
 shp.rotate(origin=origin, angleDeg=args.angle, axis=axis)
 if args.output:
-  fileFormat = 'vtk'
-  fileType = 'binary'
-  if args.ascii:
-    fileType = 'ascii'
-  if args.output.lower().find('.ply') >= 0:
-    fileFormat = 'ply'
-  shp.save(args.output, fileFormat=fileFormat, fileType=fileType)
-
+    fileFormat = 'vtk'
+    fileType = 'binary'
+    if args.ascii:
+        fileType = 'ascii'
+    if args.output.lower().find('.ply') >= 0:
+        fileFormat = 'ply'
+    shp.save(args.output, fileFormat=fileFormat, fileType=fileType)
