21f6020e3504767798a8983e1e6c57cf134e56d9
ALEXANDER PLETZER <axp5530@psu.edu>
2015-07-24 19:08:01 -0400

    flake8 compliant

--- tools/geometry/createCompositeShape.py
+++ tools/geometry/createCompositeShape.py
@@ -8,8 +8,6 @@ import argparse
 import time
 import sys
 import re
-import numpy
-from numpy import cos, sin, pi
 
 from icqsol.tools.geometry.icqShape import Shape
 
@@ -19,44 +17,45 @@ tid = re.sub(r'\.', '', str(time.time()))
 parser = argparse.ArgumentParser(description='Create composite shape.')
 
 parser.add_argument('--input', dest='input', nargs='+', default=[],
-  help='List of input files (PLY or VTK)')
+                    help='List of input files (PLY or VTK)')
 
 parser.add_argument('--compose', dest='expression',
-	help='An expression containing + (union) - (removal) and * (intersection) operations. Refer to each shape as $0, $1, etc')
+                    help='Expression with +, -,, and * on shapes $0, $1...')
 
 parser.add_argument('--ascii', dest='ascii', action='store_true',
-  help='Save data in ASCII format (default is binary)')
+                    help='Save data in ASCII format (default is binary)')
 
-parser.add_argument('--output', dest='output', 
-  default='createCompositeShape-{0}.vtk'.format(tid), help='Output file.')
+parser.add_argument('--output', dest='output',
+                    default='createCompositeShape-{0}.vtk'.format(tid),
+                    help='Output file.')
 
 args = parser.parse_args()
 
 if not args.expression:
-  print 'ERROR: must specify --compose <expression>'
-  sys.exit(2)
+    print 'ERROR: must specify --compose <expression>'
+    sys.exit(2)
 
 if len(args.input) == 0:
-  print 'ERROR: must specify at least one input file with --input <file1> <file2> ...'
-  sys.exit(3)
+    print 'ERROR: must specify input files: --input <file1> <file2> ...'
+    sys.exit(3)
 
 shp = Shape()
 argShapes = []
 for inputFile in args.input:
-  s = Shape.load(inputFile)
-  argShapes.append(s)
+    s = Shape.load(inputFile)
+    argShapes.append(s)
 
 expr = args.expression
 for i in range(len(argShapes)):
-  expr = re.sub(r'\${}'.format(i), 'argShapes[{}]'.format(i), expr)
+    expr = re.sub(r'\${}'.format(i), 'argShapes[{}]'.format(i), expr)
 compositeShape = eval(expr)
 
 if args.output:
-  fileFormat = 'vtk'
-  fileType = 'binary'
-  if args.ascii:
-    fileType = 'ascii'
-  if args.output.lower().find('.ply') >= 0:
-    fileFormat = 'ply'
-  compositeShape.save(args.output, file_format=fileFormat, file_type=fileType)
-
+    fileFormat = 'vtk'
+    fileType = 'binary'
+    if args.ascii:
+        fileType = 'ascii'
+    if args.output.lower().find('.ply') >= 0:
+        fileFormat = 'ply'
+    compositeShape.save(args.output, file_format=fileFormat,
+                        file_type=fileType)
