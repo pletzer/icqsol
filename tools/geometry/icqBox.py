b8f62926fda4494d404f24e0d96cf21b9882949a
ALEXANDER PLETZER <axp5530@psu.edu>
2015-07-24 13:30:27 -0400

    now flake8 clean

--- tools/geometry/icqBox.py
+++ tools/geometry/icqBox.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env  python
 
-import  vtk
-import  numpy
-
-from  icqShape  import  Shape
+from icqShape import Shape
 from csg.core import CSG
 
-class  Box(Shape):
 
-    def  __init__(self,  origin,  lengths):
+class Box(Shape):
+
+    def __init__(self, origin, lengths):
         """
         Constructor
         @param  radius  radius
@@ -17,18 +15,18 @@ class  Box(Shape):
         """
         center = [(origin[i] + lengths[i])/2.0 for i in range(len(origin))]
         radius = [le/2. for le in lengths]
-        shp = CSG.cube(center = center, 
-                       radius = radius)
+        shp = CSG.cube(center=center, radius=radius)
 
         Shape.__init__(self, csg=shp)
 
-################################################################################
-def  test():
+###############################################################################
 
-    box = Box(origin=(0.,  0.,  0.),  lengths=(0.5,  1.,  2.),)
-    box.save('box.vtk',  file_format='vtk',  file_type='ascii')
+
+def test():
+
+    box = Box(origin=(0.,  0.,  0.), lengths=(0.5,  1.,  2.),)
+    box.save('box.vtk', file_format='vtk', file_type='ascii')
     box.show()
 
-if  __name__ == '__main__':
+if __name__ == '__main__':
     test()
-
