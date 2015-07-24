888d03084893c5605d391624e86fd630e9f172d9
ALEXANDER PLETZER <axp5530@psu.edu>
2015-07-24 16:07:46 -0400

    now flake8 clean

--- tools/geometry/icqSphere.py
+++ tools/geometry/icqSphere.py
@@ -1,38 +1,37 @@
 #!/usr/bin/env python
 
-import vtk
-import numpy
-
 from icqShape import Shape
 from csg.core import CSG
 
+
 class Sphere(Shape):
 
-  def __init__(self, radius, origin,
-               n_theta=16, n_phi=8):
-    """
-    Constructor
-    @param radius radius
-    @param origin center of the sphere
-    @param n_theta number of theta cells
-    @param n_phi number of azimuthal cells
-    """
+    def __init__(self, radius, origin,
+                 n_theta=16, n_phi=8):
+        """
+        Constructor
+        @param radius radius
+        @param origin center of the sphere
+        @param n_theta number of theta cells
+        @param n_phi number of azimuthal cells
+        """
 
-    shp = CSG.sphere(center = origin, 
-                      radius = radius,
-                      slices = n_theta,
-                      stacks = n_phi)
+        shp = CSG.sphere(center=origin,
+                         radius=radius,
+                         slices=n_theta,
+                         stacks=n_phi)
 
-    Shape.__init__(self, csg=shp)
+        Shape.__init__(self, csg=shp)
 
-################################################################################
+##############################################################################
+
+
 def test():
 
-  sph = Sphere(radius=1.0, origin=(0., 0., 0.),
+    sph = Sphere(radius=1.0, origin=(0., 0., 0.),
                  n_theta=8, n_phi=4)
-  sph.save('sph.vtk', file_format='vtk', file_type='ascii')
-  sph.show()
+    sph.save('sph.vtk', file_format='vtk', file_type='ascii')
+    sph.show()
 
 if __name__ == '__main__':
-  test()
-
+    test()
