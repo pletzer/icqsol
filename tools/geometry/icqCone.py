534e5a90bdc587bb75c35796d2e285876ea8af99
ALEXANDER PLETZER <axp5530@psu.edu>
2015-07-24 16:15:03 -0400

    flake8 compliant

--- tools/geometry/icqCone.py
+++ tools/geometry/icqCone.py
@@ -1,42 +1,41 @@
 #!/usr/bin/env  python
 
-import  vtk
-import  numpy
-
-from  icqShape  import  Shape
+from icqShape import Shape
 from csg.core import CSG
 from csg.geom import Vector
 
-class  Cone(Shape):
 
-    def  __init__(self, radius, origin, lengths, n_theta=16):
+class Cone(Shape):
+
+    def __init__(self, radius, origin, lengths, n_theta=16):
         """
         Constructor
-        @param  radius  radius
-        @param  origin  location  of  the  focal  point
-        @param  lengths  lengths  of  the  cone  
-        @param  n_theta  number  of  theta  cells
+        @param radius radius
+        @param origin location of the focal point
+        @param lengths lengths of the cone
+        @param n_theta number of theta cells
         """
 
         ori = Vector(origin[0], origin[1], origin[2])
         end = Vector(origin[0] + lengths[0],
                      origin[1] + lengths[1],
                      origin[2] + lengths[2])
-        shp = CSG.cone(start = ori, 
-                       end = end,
-                       radius = radius,
-                       slices = n_theta)
+        shp = CSG.cone(start=ori,
+                       end=end,
+                       radius=radius,
+                       slices=n_theta)
 
         Shape.__init__(self, csg=shp)
 
-################################################################################
-def  test():
+##############################################################################
 
-    con  =  Cone(radius=1.0,  origin=(0.,  0.,  0.),  lengths=[1., 0., 0.],
-                 n_theta=8,)
-    con.save('con.vtk',  file_format='vtk',  file_type='ascii')
+
+def test():
+
+    con = Cone(radius=1.0, origin=(0.,  0.,  0.), lengths=[1., 0., 0.],
+               n_theta=8,)
+    con.save('con.vtk', file_format='vtk', file_type='ascii')
     con.show()
 
-if  __name__  ==  '__main__':
+if __name__ == '__main__':
     test()
-
