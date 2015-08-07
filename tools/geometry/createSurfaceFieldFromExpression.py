2b0b1ccd3b6b0a6e149f7acbc84be5a5df86c9f1
ALEXANDER PLETZER <axp5530@psu.edu>
2015-08-07 09:24:04 -0400

    starting work on createSurfaceFieldFromexpression

---
+++ tools/geometry/createSurfaceFieldFromExpression.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+"""
+Combine multiple shape objects
+"""
+
+import argparse
+import time
+import sys
+import re
+
+from icqsol.tools.geometry.icqShape import Shape
+
+# time stamp
+tid = re.sub(r'\.', '', str(time.time()))
+
+parser = argparse.ArgumentParser(description='Create composite shape.')
+
+parser.add_argument('--input', dest='input', nargs='+', default=[],
+                    help='List of input files (PLY or VTK)')
+
+parser.add_argument('--compose', dest='expression',
+                    help='Expression with +, -,, and * on shapes $0, $1...')
+
+parser.add_argument('--ascii', dest='ascii', action='store_true',
+                    help='Save data in ASCII format (default is binary)')
+
+parser.add_argument('--output', dest='output',
+                    default='createCompositeShape-{0}.vtk'.format(tid),
+                    help='Output file.')
+
+args = parser.parse_args()
+
+if not args.expression:
+    print 'ERROR: must specify --compose <expression>'
+    sys.exit(2)
+
+if len(args.input) == 0:
+    print 'ERROR: must specify input files: --input <file1> <file2> ...'
+    sys.exit(3)
+
+shp = Shape()
+argShapes = []
+for inputFile in args.input:
+    s = Shape.load(inputFile)
+    argShapes.append(s)
+
+expr = args.expression
+for i in range(len(argShapes)):
+    expr = re.sub(r'\${}'.format(i), 'argShapes[{}]'.format(i), expr)
+compositeShape = eval(expr)
+
+if args.output:
+    fileFormat = 'vtk'
+    fileType = 'binary'
+    if args.ascii:
+        fileType = 'ascii'
+    if args.output.lower().find('.ply') >= 0:
+        fileFormat = 'ply'
+    compositeShape.save(args.output, file_format=fileFormat,
+                        file_type=fileType)
