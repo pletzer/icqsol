e3d3746cfca9b29a9f9d9b87ca1a2d3b48f6cce7
ALEXANDER PLETZER <axp5530@psu.edu>
2015-08-17 20:56:52 -0400

    adding more tests

---
+++ tests/tools/geometry/testPointInsideShape.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+"""
+Test for determining whether a point is inside a shape 
+@author pletzer@psu.edu
+"""
+
+import numpy
+import argparse
+from icqsol.tools.geometry.icqSphere import Sphere
+from icqsol.tools.geometry.icqInside import Inside
+
+parser = argparse.ArgumentParser(description='Test point inside shape')
+
+parser.add_argument('--n_theta', type=int, dest='n_theta', default=8,
+                    help='Set number of poloidal cells')
+parser.add_argument('--n_phi', type=int, dest='n_phi', default=4,
+                    help='Set number of azimuthal cells')
+args = parser.parse_args()
+
+shp = Sphere(radius=1.0, origin=(0., 0., 0.),
+           n_theta=args.n_theta, n_phi=args.n_phi)
+
+inside = Inside(shp)
+    
+pt = numpy.array([0., 0., 0.])
+assert(inside.isInside(pt, 0.) == 1)
+
+pt = numpy.array([1.01, 0., 0.])
+assert(inside.isInside(pt, 0.) == -1)
+
+pt = numpy.array([0., 0.9999999999, 0.])
+assert(inside.isInside(pt, 0.0) == 1)
+assert(inside.isInside(pt, 0.01) == 0)
+
+pt = numpy.array([0., 1.0000000001, 0.])
+assert(inside.isInside(pt, 0.) == -1)
+assert(inside.isInside(pt, 0.01) == 0)
