8e34cf552c5fc112070f6c27574d20d2c966f151
ALEXANDER PLETZER <axp5530@psu.edu>
2015-10-14 10:46:22 -0600

    working on the quadrature part

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(name='icqsol',
       package_dir = {'icqsol': ''}, # the present working directory maps to icqsol below
       packages=['icqsol',
                 'icqsol.color', 
+                'icqsol.quadrature',
                 'icqsol.discretization',
                 'icqsol.shapes',
                 'icqsol.util'],
