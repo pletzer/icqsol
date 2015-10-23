0d0b97f430c862a342dbcab009663877adb798a5
ALEXANDER PLETZER <axp5530@psu.edu>
2015-10-23 09:15:29 -0600

    + exp test

--- tests/testQuadrature.py
+++ tests/testQuadrature.py
@@ -7,17 +7,21 @@ Test Gauss quadrature
 
 import unittest
 import numpy
+from math import exp, e
 from icqsol.bem.icqQuadrature import triangleQuadrature
 
 class TestQuadrature(unittest.TestCase):
 
     def testTrianglePolynomial(self):
+
         pa = numpy.array([0., 0., 0.])
         pb = numpy.array([1., 0., 0.])
         pc = numpy.array([0., 1., 0.])
-        def func(x):
-            x, y, z = x
+
+        def func(point):
+            x, y, z = point
             return 1. + 2*x + 3*y**2
+
         exact = 13./12.
         
         order = 1
@@ -33,14 +37,17 @@ class TestQuadrature(unittest.TestCase):
             assert(abs(value - exact) < 1.e-10)
 
     def testTrianglePolynomial2(self):
+
         pa = numpy.array([1., 2., 3.])
         pb = numpy.array([1.1, 2.3, 3.4])
         pc = numpy.array([0.8, 1., 0.])
         jacVec = numpy.cross(pb - pa, pc - pa)
         jac = numpy.sqrt(jacVec.dot(jacVec))
-        def func(x):
-            x, y, z = x
+
+        def func(point):
+            x, y, z = point
             return 1. + 2*x + 3*y**2 + 3*z**3
+
         exact = jac*780053./30000.
     
         order = 1
@@ -63,6 +70,28 @@ class TestQuadrature(unittest.TestCase):
                                        func=func)
             error = value - exact
             assert(abs(value - exact) < 1.e-12)
+
+    def testTriangleExp(self):
+
+        pa = numpy.array([1., 2., 3.])
+        pb = numpy.array([1.1, 2.3, 3.4])
+        pc = numpy.array([0.8, 1., 0.])
+        jacVec = numpy.cross(pb - pa, pc - pa)
+        jac = numpy.sqrt(jacVec.dot(jacVec))
+
+        def func(point):
+            x, y, z = point
+            return exp(x)
+
+        exact = jac*50/3*(e**(4./5.)-3*e + 2*e**(11./10.))
+            
+        for order in range(1, 9):
+            value = triangleQuadrature(order=order,
+                                       pa=pa, pb=pb, pc=pc,
+                                       func=func)
+            error = value - exact
+            print 'order = {} value = {} error = {}'.format(order, value, error)
+            assert(abs(value - exact) < 0.002)
 
 
 if __name__ == '__main__':
