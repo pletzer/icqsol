64d058a04b61b9a1eead7e5c3843beff7a2b9a7e
ALEXANDER PLETZER <axp5530@psu.edu>
2016-01-05 17:39:13 -0700

    convert point data to cell data automatically, if need be

--- bem/icqLaplaceMatrices.py
+++ bem/icqLaplaceMatrices.py
@@ -48,20 +48,60 @@ class LaplaceMatrices:
         self.order = order
         self.__computeMatrices()
         
+    def getArrayIndexFromName(self, data, name):
+        """
+        Get the array index from its name
+        @param data either a vtkCellData or a vtkPointData object
+        @param name name
+        @return index >= 0 if name exists or -1 if it does not
+        """
+        numArrays = data.GetNumberOfArrays()
+        index = -1
+        for i in range(numArrays):
+            arr = data.GetArray(i)
+            if arr.GetName() == name:
+                index = i
+                break
+        return index
+
     def getPotentialArrayIndexFromName(self, name):
         """
-        Get the potential field array index from its name
+        Get the potential array index from its name, if the array 
+        is a point data type then project onto cells
         @param name name
         @return index >= 0 if name exists or -1 if it does not
         """
         cellData = self.pdata.GetCellData()
-        numArrays = cellData.GetNumberOfArrays()
-        index = -1
-        for i in range(numArrays):
-            arr = cellData.GetArray(i)
-            if arr.GetName() == name:
-                index = i
-                break
+        pointData = self.pdata.GetPointData()
+        index = self.getArrayIndexFromName(cellData, name)
+        if index < 0:
+            # Maybe a point array?
+            index2 = self.getArrayIndexFromName(self, pointData, name)
+
+            if index2 >= 0:
+                # Project from points to cells
+                pointArr = pointData.GetArray(index2)
+                numCells = self.pdata.GetNumberOfPolys()
+                cellArr = vtk.vtkDoubleArray()
+                cellArr.SetName(name) # same name as the point array
+                cellArr.SetNumberOfComponents(1)
+                cellArr.SetNumberOfTuples(numCells)
+                cells = self.pdata.GetPolys()
+                ptIds = vtk.vtkIdList()
+                cells.InitTraversal()
+                for i in range(numCells):
+                    cells.GetNextCell(ptIds)
+                    # We know for sure that all cells are triangles
+                    ia, ib, ic = ptIds.GetId(0), \
+                        ptIds.GetId(1), ptIds.GetId(2)
+                    va = pointArr.GetComponent(ia, 0)
+                    vb = pointArr.GetComponent(ib, 0)
+                    vc = pointArr.GetComponent(ic, 0)
+                    cellArr.SetComponent(i, 0, (va + vb + vc)/3.)
+                # Add the cell array
+                cellData.AddArray(cellArr)
+                return self.getPotentialArrayIndexFromName(self, name)
+
         return index
 
     def setPotentialFromExpression(self, expression, potName='v'):
