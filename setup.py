fe25a3e8c5326db5da1f51be9af05de988a3b39e
ALEXANDER PLETZER <axp5530@psu.edu>
2016-01-08 18:42:44 -0700

    version update

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ pletzer@psu.edu
 from setuptools import setup, Extension
 
 setup(name='icqsol',
-      version='0.3.7.1',
+      version='0.3.8',
       description='Solving engineering problems on the web',
       author='Alex Pletzer and Greg Von Kuster',
       author_email='alexander@gokliya.net',
