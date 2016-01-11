05e8a2ed76942099dc1e8d6c6470d10987f91ef3
ALEXANDER PLETZER <axp5530@psu.edu>
2016-01-11 11:35:53 -0700

    working on a C++ implementation of the laplace matrix construction

---
+++ bem/icqLaplaceMatrices.cpp
@@ -0,0 +1,126 @@
+#include <vector>
+#include <cmath>
+#include <icqQuadrature.hpp>
+#include <icqLaplaceMatrices.hpp>
+#include <vtkCellArray.h>
+#include <vtkPoints.h>
+#include <vtkIdList.h>
+
+/**
+ * Compute the off diagonal influence matrix elements
+ * @param pdata vtkPolyData instance
+ * @param gMat influence matrix
+ */
+extern "C"
+void computeOffDiagonalTerms(vtkPolyData* pdata, double* gMat) {
+
+    icqQuadratureType* self;
+    icqQuadratureInit(&self);
+    int maxOrder = icqQuadratureGetMaxOrder(&self);
+
+    // Vertex coordinates for the source triangle
+    std::vector<double> paSrc(3);
+    std::vector<double> pbSrc(3);
+    std::vector<double> pcSrc(3);
+
+    // Edges for the source triangle
+    std::vector<double> pb2Src(3);
+    std::vector<double> pc2Src(3);
+
+    // Source area vector
+    std::vector<double> areaSrcVec(3);
+
+    // Observer triangle vertices
+    std::vector<double> paObs(3);
+    std::vector<double> pbObs(3);
+    std::vector<double> pcObs(3);
+
+    // Triangle mid-points
+    std::vector<double> xObs(3);
+    std::vector<double> xSrc(3);
+
+    vtkCellArray* polys = pdata->GetPolys();
+    vtkPoints* points = pdata->GetPoints();
+    size_t numTriangles = polys->GetNumberOfCells();
+    polys->InitTraversal();
+
+    // Build the connectivity array, all triangles
+    vtkIdList* ptIds = vtkIdList::New();
+    std::vector<int> cells(numTriangles*3);
+    for (size_t i = 0; i < numTriangles; ++i) {
+        polys->GetNextCell(ptIds);
+        size_t ia = ptIds->GetId(0);
+        size_t ib = ptIds->GetId(1);
+        size_t ic = ptIds->GetId(2);
+        cells[3*i + 0] = ia;
+        cells[3*i + 1] = ib;
+        cells[3*i + 2] = ic; 
+    }
+    ptIds->Delete();
+
+    // iterate over the source triangles
+    for (size_t jSrc = 0; jSrc < numTriangles; ++jSrc) {
+
+        size_t ia = cells[3*jSrc + 0];
+        size_t ib = cells[3*jSrc + 1];
+        size_t ic = cells[3*jSrc + 2];
+
+        points->GetPoint(ia, &paSrc[0]);
+        points->GetPoint(ib, &pbSrc[0]);
+        points->GetPoint(ic, &pcSrc[0]);
+
+        // The triangle's normal vector and area at the center of the triangle
+        for (size_t j = 0; j < 3; ++j) {
+            pb2Src[j] = pbSrc[j] - paSrc[j];
+            pc2Src[j] = pcSrc[j] - paSrc[j];
+            xSrc[j] = (paSrc[j] + pbSrc[j] + pcSrc[j])/3.;
+        }
+        for (size_t j = 0; j < 3; ++j) {
+            size_t j1 = (j + 1) % 3;
+            size_t j2 = (j + 2) % 3;
+            areaSrcVec[j] = pb2Src[j1]*pc2Src[j2] - pb2Src[j2]*pc2Src[j1];
+        }
+        double areaSrc = 0;
+        for (size_t j = 0; j < 3; ++j) {
+            areaSrc += areaSrcVec[j]*areaSrcVec[j];
+        }
+        areaSrc = sqrt(areaSrc);
+
+        // iterate over the observer triangles
+        for (size_t iObs = 0; iObs < numTriangles; ++iObs) {
+
+            if (iObs == jSrc) {
+                // singular term
+                continue;
+            }
+
+            points->GetPoint(cells[3*iObs + 0], &paObs[0]);
+            points->GetPoint(cells[3*iObs + 1], &pbObs[0]);
+            points->GetPoint(cells[3*iObs + 2], &pcObs[0]);
+
+            // Observer is at mid point
+            for (size_t j = 0; j < 3; ++j) {
+                xObs[j] = (paObs[j] + pbObs[j] + pcObs[j])/3.;
+            }
+
+            icqQuadratureSetObserver(&self, &xObs[0]);
+
+            // Gauss quadrature order estimate
+            double normDistance = 0;
+            for (size_t j = 0; j < 3; ++j) {
+                normDistance += (xObs[j] - xSrc[j])*(xObs[j] - xSrc[j]);
+            }
+            normDistance = sqrt(normDistance) / sqrt(areaSrc);
+            int offDiagonalOrder = (int)(maxOrder * 2 / normDistance);
+            offDiagonalOrder = std::min(maxOrder, std::max(1, offDiagonalOrder));
+
+            gMat[numTriangles*iObs + jSrc] = icqQuadratureEvaluate(&self, 
+                                                                   offDiagonalOrder, 
+                                                                   &paSrc[0], &pbSrc[0], &pcSrc[0]);
+        }
+
+    }
+
+    icqQuadratureDel(&self);
+
+}
--- setup.py
+++ setup.py
@@ -5,7 +5,57 @@ Set up script for icqsol
 pletzer@psu.edu
 """
 
+import os
 from setuptools import setup, Extension
+
+
+def getVtkLibraryDirs():
+    """
+    Guess the path of the VTK libraries 
+    @return list of paths
+    """
+    
+    # Sniff LD_LIBRARY_PATH (or equivalent)
+    ld_library_path_name = 'LD_LIBRARY_PATH'
+    separator = ':'
+    shared_library_suffix = 'so'
+    uname = os.uname()[0]
+    
+    if uname == 'Darwin':
+        ld_library_path_name = 'DYLD_LIBRARY_PATH'
+        shared_library_suffix = 'dylib'
+    elif uname == 'Windows':
+        # untested!
+        ld_library_path_name = 'PATH'
+        separator = ';'
+        shared_library_suffix = 'dll'
+
+    library_dirs = []
+    for lib_dir in os.environ.get(ld_library_path_name, '').split(separator):
+        if os.path.exists(lib_dir + '/libvtkCommon.' + shared_library_suffix):
+            library_dirs.append(lib_dir)
+
+    return library_dirs
+
+def getVtkIncludeDirs():
+    """
+    Guess the path of the VTK include files
+    @return list of paths
+    """
+
+    library_dirs = getVtkLibraryDirs()
+    include_dirs = []
+    for path in library_dirs:
+
+        if len(path) > 0 and path[-1] == '/': 
+            path = path[:-1]
+
+        bn = os.path.basename(path)
+        if os.path.exists(path + '/../include'):
+            include_dirs.append(path + '/../include')
+        elif os.path.exists(path + '/../../include/' + bn):
+            include_dirs.append(path + '/../../include/' + bn)
+    return include_dirs
 
 setup(name='icqsol',
       version='0.3.8',
@@ -14,8 +64,8 @@ setup(name='icqsol',
       author_email='alexander@gokliya.net',
       url='https://github.com/pletzer/icqsol/wiki',
       install_requires=['pytriangle>=1.0.6', 'pycsg>=0.2.3'], # should really be 'pycsg>=0.3.3'],
-      dependency_links=['http://github.com/pletzer/pytriangle/tarball/master#egg=pytriangle-1.0.3',
-                        'http://github.com/pletzer/pycsg/tarball/master#egg=pycsg-0.2.1',
+      dependency_links=['http://github.com/pletzer/pytriangle/tarball/master#egg=pytriangle-1.0.6',
+                        'http://github.com/pletzer/pycsg/tarball/master#egg=pycsg-0.3.3',
       ],
       package_dir = {'icqsol': ''}, # the present working directory maps to icqsol below
       data_files = [('icqsol/textures', ['textures/Swietenia_macrophylla_wood.jpg',
@@ -31,9 +81,14 @@ setup(name='icqsol',
                                ['bem/icqFunctor.cpp',
                                 'bem/icqLaplaceFunctor.cpp',
                                 'bem/icqQuadrature.cpp'],
-                               define_mactros=[], 
                                include_dirs=['bem'],
                                ),
+                    Extension('icqsol.icqLaplaceMatricesCpp', 
+                              ['bem/icqLaplaceMatrices.cpp'],
+                              include_dirs=['bem'] + getVtkIncludeDirs(),
+                              library_dirs=getVtkLibraryDirs(),
+                              libraries=['vtkCommon', 'vtkFiltering'],
+                              )
       ],
       requires = ['numpy', 'vtk',],
      )
