5d3024c3df979b8ddb15c69bde69c18242878761
pletzera <pletzera@niwa-1007638.niwa.local>
2016-03-20 14:26:55 +1300

    updating results to the latest version

--- checkOutput.sh
+++ checkOutput.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env 
 
+good="yes"
 for f in `ls *.vtk`; do 
     if [ -f "test_results/$f" ]; then
-        diff $f test_results/$f
+        diff $f test_results/$f > /dev/null
         if [ $? != 0 ]; then
             echo "ERROR: $f and test_results/$f are different"
-            exit 1
+            good="no"
         fi
     fi
 done
 
 for f in `ls *.ply`; do
     if [ -f "test_results/$f" ]; then
-        diff $f test_results/$f
+        diff $f test_results/$f > /dev/null
         if [ $? != 0 ]; then
             echo "ERROR: $f and test_results/$f are different"
-            exit 1
+            good="no"
         fi
     fi
 done
 
+if [ $good == "no" ]; then
+    exit 1
+fi
