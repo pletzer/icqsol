b559d0458d775740f4d450dcc8544c2f39e51efd
pletzera <pletzera@niwa-1007638.niwa.local>
2016-04-05 16:38:09 +1200

    working on smoothing filter

---
+++ shapes/icqSmoothSurface.py
@@ -0,0 +1,232 @@
+#!/usr/bin/env python
+
+import math
+import numpy
+import vtk
+
+class SmoothSurface:
+
+    def __init__(self, pdata):
+        """
+        Constructor
+        @param pdata vtkPolyData instance
+        """
+        self.polydata = pdata
+
+        # save the points and point data as separate class members
+        # so as not to pollute pdata
+        self.points = vtk.vtkPoints()
+        self.points.DeepCopy(pdata.GetPoints())
+
+        self.pointData = {}
+        pd = pdata.GetPointData()
+        for i in range(pd.GetNumberOfArrays()):
+            arr = pd.GetArray(i)
+            name = arr.GetName()
+            self.pointData[name] = vtk.vtkDoubleArray()
+            self.pointData[name].DeepCopy(arr)
+
+        self.cellData = {}
+        cd = pdata.GetCellData()
+        for i in range(cd.GetNumberOfArrays()):
+            name = cd.GetArray(i).GetName()
+            self.cellData[name] = vtk.vtkDoubleArray()
+            self.cellData[name].SetName(name)
+
+    def getVtkPolyData(self):
+        """
+        Get the vtkPolyData instance associated with the grid
+        @return vtkPolyData instance
+        """
+        return self.polydata
+
+    def smooth(self, min_cell_area = 1.e-10):
+        """
+        Smooth surface by removing very small cells
+        @param min_cell_area cell area tolerance
+        @note operation is in place
+        """
+
+        pd = self.polydata.GetPointData()
+        numArrays = pd.GetNumberOfArrays()
+
+        points = self.pdata.GetPoints()
+        polys = self.pdata.GetPolys()
+        p = numpy.zeros((3,), numpy.float64)
+        numPolys = polys.GetNumberOfCells()
+        ptIds = vtk.vtkIdList()
+
+        # holds the cell Ids to delete
+        cellIdsToRemove = []
+
+        # iterate over cells
+        polys.InitTraversal()
+        for i in range(numPolys):
+
+            polys.GetNextCell(ptIds)
+
+            if self.getPolygonArea(ptIds) < min_cell_area:
+
+                # number of points spanning the cell
+                n = ptIds.GetNumberOfIds()
+
+                # compute the cell's gravity center
+                barycenter = numpy.zeros((3,), numpy.float64)
+                for j in range(n):
+                    p[:] = points.GetPoint(ptId)
+                    barycenter += p
+                barycenter /= float(n)
+
+                # interpolate the nodal fields to the barycenter locations
+                for el in range(numArrays):
+                    arr = pd.GetArray(el)
+                    numComps = arr.GetNumberOfComponents()
+                    vals = numpy.zeros((numComps,), numpy.float64)
+                    baryVals = numpy.zeros((numComps,), numpy.float64)
+                    # mid cell values
+                    for j in range(n):
+                        vals[:] = arr.GetTuple(ptIds.GetId(j))
+                        baryVals += vals
+                    baryVals /= float(n)
+                    # set the field values to the mid cell values
+                    for j in range(n):
+                        arr.SetTuple(ptIds.GetId(j), baryVals)
+
+                # tag the cell for removal
+                cellIdsToRemove.append(i)
+        
+        # remove the tagged, zero-area cells
+        for cellId in cellIdsToRemove:
+            self.pdata.DeleteCell(cellId)
+
+        # now remove
+        self.pdata.RemoveDeletedCells()
+
+    def getPolygonArea(self, polyPtIds):
+        """
+        Compute the (scalar) area of a polygon
+        @param polyPtIds list of point indices
+        @return area
+        """
+        area = numpy.zeros((3,), numpy.float64)
+        p0 = numpy.array(self.points.GetPoint(polyPtIds[0]))
+        numPolyPts = len(polyPtIds)
+        for i in range(1, numPolyPts - 1):
+            p1 = numpy.array(self.points.GetPoint(polyPtIds[i]))
+            p2 = numpy.array(self.points.GetPoint(polyPtIds[i + 1]))
+            area += numpy.cross(p1 - p0, p2 - p0)
+        return numpy.linalg.norm(area)
+
+    def removeDegeneratePoints(self, polyPtIds):
+        """
+        Remove degenerate points
+        @param polyPtIds list of point indices (modified on output)
+        """
+        indicesToDelete = []
+        numPolyPts = len(polyPtIds)
+        for i in range(numPolyPts):
+            p0 = numpy.array(self.points.GetPoint(polyPtIds[i]))
+            p1 = numpy.array(self.points.GetPoint(polyPtIds[(i + 1)%numPolyPts]))
+            if numpy.linalg.norm(p1 - p0) < 1.e-15:
+                indicesToDelete.append(i)
+        for j in range(len(indicesToDelete) - 1, -1, -1):
+            i = indicesToDelete[j]
+            del polyPtIds[i]
+
+##############################################################################
+
+
+def printVtkPolyData(pdata):
+
+    points = pdata.GetPoints()
+    polys = pdata.GetPolys()
+    ptIds = vtk.vtkIdList()
+    numPolys = polys.GetNumberOfCells()
+    print 'Number of polygons: {0}'.format(numPolys)
+    polys.InitTraversal()
+    for i in range(numPolys):
+        numPts = ptIds.GetNumberOfIds()
+        print '\tCell {0} has {1} points: '.format(i, numPts)
+        for j in range(numPts):
+            ptId = ptIds.GetId(j)
+            pt = points.GetPoint(ptId)
+            print '\t\t{0} -> {1}'.format(ptId, pt)
+
+
+def testNoRefinement():
+
+    points = vtk.vtkPoints()
+    points.InsertNextPoint((0., 0., 0.))
+    points.InsertNextPoint((1., 0., 0.))
+    points.InsertNextPoint((1., 1., 0.))
+
+    pdata = vtk.vtkPolyData()
+    pdata.SetPoints(points)
+
+    ptIds = vtk.vtkIdList()
+    ptIds.InsertNextId(0)
+    ptIds.InsertNextId(1)
+    ptIds.InsertNextId(2)
+    pdata.Allocate(1, 1)
+    pdata.InsertNextCell(vtk.VTK_POLYGON, ptIds)
+
+    rs = SmoothSurface(pdata)
+    rs.smooth(min_cell_area=1.e-5)
+    pdata2 = rs.getVtkPolyData()
+    assert(pdata2.GetNumberOfPolys() == 1)
+
+
+def testAddingThreePointsThenMore():
+
+    points = vtk.vtkPoints()
+    points.InsertNextPoint((0., 0., 0.))
+    points.InsertNextPoint((2., 0., 0.))
+    points.InsertNextPoint((2., 1., 0.))
+
+    pdata = vtk.vtkPolyData()
+    pdata.SetPoints(points)
+
+    ptIds = vtk.vtkIdList()
+    ptIds.InsertNextId(0)
+    ptIds.InsertNextId(1)
+    ptIds.InsertNextId(2)
+    pdata.Allocate(1, 1)
+    pdata.InsertNextCell(vtk.VTK_POLYGON, ptIds)
+
+    rs = SmoothSurface(pdata)
+    rs.smooth(min_cell_area=1.e-5)
+    assert(rs.getVtkPolyData().GetNumberOfPolys() == 4)
+
+
+def testStartingWithTwoCells():
+
+    points = vtk.vtkPoints()
+    points.InsertNextPoint((0., 0., 0.))
+    points.InsertNextPoint((2., 0., 0.))
+    points.InsertNextPoint((2., 1., 0.))
+    points.InsertNextPoint((0., 1., 0.))
+
+    pdata = vtk.vtkPolyData()
+    pdata.SetPoints(points)
+
+    pdata.Allocate(2, 1)
+    ptIds = vtk.vtkIdList()
+    ptIds.SetNumberOfIds(3)
+    ptIds.SetId(0, 0)
+    ptIds.SetId(1, 1)
+    ptIds.SetId(2, 2)
+    pdata.InsertNextCell(vtk.VTK_POLYGON, ptIds)
+    ptIds.SetId(0, 2)
+    ptIds.SetId(1, 3)
+    ptIds.SetId(2, 0)
+    pdata.InsertNextCell(vtk.VTK_POLYGON, ptIds)
+
+    rs = SmoothSurface(pdata)
+    rs.smooth(min_cell_area=1.e-5)
+    assert(rs.getVtkPolyData().GetNumberOfPolys() == 8)
+
+
+if __name__ == '__main__':
+    testNoRefinement()
+    testAddingThreePointsThenMore()
+    testStartingWithTwoCells()
