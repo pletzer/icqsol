e2daefe0694595d2475548b3910642d7f745a676
ALEXANDER PLETZER <axp5530@psu.edu>
2016-05-16 19:58:27 +1200

    now builds under python 3.5

--- setup.py
+++ setup.py
@@ -7,7 +7,34 @@ alexander@gokliya.net
 
 import os
 from setuptools import setup, Extension
+import re
+import vtk # for version number
 import __init__ # for version number
+
+def anacondaLibraryNameFix(vtkLibraries, vtkLibraryDir):
+    """
+    On Anaconda, some of the libraries only exist with the vtk version 
+    appended.
+    """
+    vtkVersion = '-{0}.{1}'.format(vtk.VTK_MAJOR_VERSION, vtk.VTK_MINOR_VERSION)
+    suffix = '.so'
+    if os.uname()[0] == 'Darwin':
+        suffix = '.dylib'
+    elif os.uname()[0] == 'Windows':
+        # I don't have a windows machine to test...
+        # do we have to worry about capitalization on Windows?
+        suffix = '.dll'
+    count = 0
+    for lib in vtkLibraries:
+        if not os.path.exists(vtkLibraryDir + '/lib' + lib + suffix):
+            newName = lib + vtkVersion
+            li = vtkLibraryDir + '/lib' + newName + suffix
+            if os.path.exists(vtkLibraryDir + '/lib' + newName + suffix) or \
+               os.path.exists(vtkLibraryDir + '/lib' + newName + '.a') or \
+               os.path.exists(vtkLibraryDir + '/' + newName + suffix):
+                # set the loibrary name to name-MAJOR_VERSION.MINOR_VERSION
+                vtkLibraries[count] = newName
+        count += 1
 
 # Because we're linking C++ code to the VTK library, we 
 # need to know where VTK was installed. 
@@ -15,11 +42,10 @@ VTK_INCLUDE_DIRS = os.environ.get('VTK_INCLUDE_DIRS', '').split(';')
 VTK_LIBRARIES = os.environ.get('VTK_LIBRARIES', '').split(';')
 VTK_RUNTIME_LIBRARY_DIRS = os.environ.get('VTK_RUNTIME_LIBRARY_DIRS', '').split(';')
 
-# OpenMP flags
-OPENMP_COMPILER_ARG = os.environ.get('OPENMP_COMPILER_ARG', '-fopenmp')
-OPENMP_COMPILER_LIB = os.environ.get('OPENMP_COMPILER_LIB', 'gomp')
+# Anaconda renamed the libraries
+anacondaLibraryNameFix(VTK_LIBRARIES, VTK_RUNTIME_LIBRARY_DIRS[0])
 
-# remove all the Python libraries since they are causing a link issue 
+# remove all the Python libraries since they are causing a link issue
 # on CentOS 7 and we don't really need them
 indicesToDelete = []
 for i in range(len(VTK_LIBRARIES)):
@@ -31,9 +57,9 @@ indicesToDelete.reverse()
 for i in indicesToDelete:
   del VTK_LIBRARIES[i]
 
-print 'VTK_INCLUDE_DIRS = ', VTK_INCLUDE_DIRS
-print 'VTK_LIBRARIES = ', VTK_LIBRARIES
-print 'VTK_RUNTIME_LIBRARY_DIRS = ', VTK_RUNTIME_LIBRARY_DIRS
+print('VTK_INCLUDE_DIRS         = {0}\n'.format(VTK_INCLUDE_DIRS))
+print('VTK_LIBRARIES            = {0}\n'.format(VTK_LIBRARIES))
+print('VTK_RUNTIME_LIBRARY_DIRS = {0}\n'.format(VTK_RUNTIME_LIBRARY_DIRS))
 
 setup(name='icqsol',
       version=__init__.__version__, 
@@ -41,9 +67,9 @@ setup(name='icqsol',
       author='Alex Pletzer and Greg Von Kuster',
       author_email='alexander@gokliya.net',
       url='https://github.com/pletzer/icqsol/wiki',
-      install_requires=['pytriangle>=1.0.6', 'pycsg>=0.3.6'],
-      dependency_links=['http://github.com/pletzer/pytriangle/tarball/master#egg=pytriangle-1.0.6',
-                        'http://github.com/pletzer/pycsg/tarball/master#egg=pycsg-0.3.6',
+      install_requires=['pytriangle>=1.0.7', 'pycsg>=0.3.7'],
+      dependency_links=['http://github.com/pletzer/pytriangle/tarball/master#egg=pytriangle-1.0.7',
+                        'http://github.com/pletzer/pycsg/tarball/master#egg=pycsg-0.3.7',
       ],
       package_dir = {'icqsol': ''}, # the present working directory maps to icqsol below
       data_files = [('icqsol/textures', ['textures/Swietenia_macrophylla_wood.jpg',
@@ -62,9 +88,8 @@ setup(name='icqsol',
                                 'bem/icqQuadrature.cpp',
                                 'bem/icqLaplaceMatrices.cpp'],
                                include_dirs=['bem'] + VTK_INCLUDE_DIRS,
-                               extra_compile_args=[OPENMP_COMPILER_ARG],
                                library_dirs=VTK_RUNTIME_LIBRARY_DIRS,
-                               libraries=VTK_LIBRARIES + [OPENMP_COMPILER_LIB],
+                               libraries=VTK_LIBRARIES,
                                ),
                       Extension('icqsol.icqInsideLocatorCpp', 
                                 ['csg/icqInsideLocator.cpp'],
