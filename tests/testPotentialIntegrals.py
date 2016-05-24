328ef9242c767f6bd0587fcdbb7893defa6c7a5e
Alex Pletzer <alexander@gokliya.net>
2016-05-24 18:02:58 +1200

    + test another location

--- tests/testPotentialIntegrals.py
+++ tests/testPotentialIntegrals.py
@@ -3,17 +3,30 @@
 from icqsol.bem.icqPotentialIntegrals import PotentialIntegrals
 import numpy
 
-def test1(order):
+def testObserverOnA(order):
     paSrc = numpy.array([0., 0., 0.])
     pbSrc = numpy.array([1., 0., 0.])
     pcSrc = numpy.array([0., 1., 0.])
     xObs = paSrc
     integral = PotentialIntegrals(xObs, pbSrc, pcSrc, order).getIntegralOneOverR()
     exact = numpy.sqrt(2.) * numpy.arcsinh(1.)
-    print('test 1: order = {0} integral = {1} exact = {2} error = {3}'.format(\
+    print('testObserverOnA: order = {0} integral = {1} exact = {2} error = {3}'.format(\
         order, integral, exact, integral - exact))
+
+def testObserverOnB(order):
+    paSrc = numpy.array([0., 0., 0.])
+    pbSrc = numpy.array([1., 0., 0.])
+    pcSrc = numpy.array([0., 1., 0.])
+    xObs = pbSrc
+    integral = PotentialIntegrals(xObs, pcSrc, paSrc, order).getIntegralOneOverR()
+    exact = numpy.arcsinh(1.)
+    print('testObserverOnB: order = {0} integral = {1} exact = {2} error = {3}'.format(\
+        order, integral, exact, integral - exact))
+
 
 if __name__ == '__main__':
     for order in range(1, 6):
-        test1(order)
+        testObserverOnA(order)
+        testObserverOnB(order)
+        print('-'*80)
 
