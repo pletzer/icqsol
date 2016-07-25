70fb77548200607e03e8c78bfa0252a4a2ca5f18
Alex Pletzer <alexander@gokliya.net>
2016-07-25 21:36:32 +1200

    cleaning up some tests

--- tests/testGKMatrices.py
+++ tests/testGKMatrices.py
@@ -2,36 +2,25 @@
 
 from __future__ import print_function
 from icqsol.shapes.icqShapeManager import ShapeManager
-from icqsol.bem.icqLaplaceMatrices import LaplaceMatrices
+from icqsol.bem.icqLaplaceSolver import LaplaceSolver
 from icqsol import util
 import numpy
 
 shape_mgr = ShapeManager(file_format='vtk', vtk_dataset_type='POLYDATA')
 pdata = shape_mgr.loadAsVtkPolyData('data/2triangles.vtk')
-response = LaplaceMatrices(pdata,
-                           max_edge_length=float('inf'),
-                           order=5)
-g = response.getGreenMatrix()
-k = response.getNormalDerivativeGreenMatrix()
+slv = LaplaceSolver(pdata,
+                    max_edge_length=float('inf'),
+                    order=5)
+g = slv.getGreenMatrix()
 print('g = ')
 print g
-print('k = ')
-print k
 
 gExact = numpy.array([[0.191561, 0.0378501], [0.0378501, 0.191561]])
-kExact = numpy.array([[0, -0.0344229], [-0.0344229, 0]])
 print('g Mathematica = ')
 print(gExact)
-print('k Mathematica = ')
-print(kExact)
 
 print('g error = ')
 print(g - gExact)
-print('k error = ', end="")
-print(k - kExact)
 
 print('g^{-1} = ', numpy.linalg.inv(g))
 
-# residue
-k[0, 0] = -0.5
-k[1, 1] = -0.5
--- tests/testGMatrix.py
+++ tests/testGMatrix.py
@@ -2,22 +2,16 @@
 
 from __future__ import print_function
 from icqsol.shapes.icqShapeManager import ShapeManager
-from icqsol.bem.icqLaplaceMatrices import LaplaceMatrices
+from icqsol.bem.icqLaplaceSolver import LaplaceSolver
 from icqsol import util
 import numpy
 
 shape_mgr = ShapeManager(file_format='vtk', vtk_dataset_type='POLYDATA')
 pdata = shape_mgr.loadAsVtkPolyData('data/tet.vtk')
-response = LaplaceMatrices(pdata,
-                           max_edge_length=float('inf'),
-                           order=5)
-g = response.getGreenMatrix()
-k = response.getNormalDerivativeGreenMatrix()
-
-# residue
-n = k.shape[0]
-for i in range(n):
-    k[i, i] += 0.5
+slv = LaplaceSolver(pdata,
+                    max_edge_length=float('inf'),
+                    order=5)
+g = slv.getGreenMatrix()
 
 print('g:')
 print g
@@ -28,19 +22,6 @@ print(gM1)
 
 print('g^{-1} * g: ')
 print(gM1.dot(g))
-
-print('k + residue: ')
-print k
-
-print('kres eigenvalues')
-print(numpy.linalg.eig(k))
-
-print('g^{-1} * kres: ')
-response = gM1.dot(k)
-print(gM1.dot(k))
-
-print('g^{-1} * kres * 1: ')
-print(response.dot([1.,]*n))
 
 
 
